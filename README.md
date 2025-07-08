4bbb9e164bd9af6ad5dd98bd6342f1a4cf5a142c
Janell Huyck <janell.huyck@outlook.com>
2025-07-07 20:50:27 -0400

    Working craps game

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ o	There is an initial roll of 2 dice resulting in a sum of 2 – 12.
 2)	If the sum is 7 or 11 it is called a ‘natural’ and the game is over with a win.
 3)	For all other values, the sum becomes ‘the point’ and the user makes subsequent rolls until they either throw a 7 in which case they lose or they make the point sum in which case they win.
 - After a win or loss, the next player rolls the die for a new game. In our simulation, the program will simply ask the user if they want to continue to play.
--	For each roll show the value for each die and the sum.  
--	For case i or ii above, indicate that the user either crapped out or won with a natural and then prompt to play again.
--	For case iii indicate that the sum is now the point.  For each subsequent roll indicate the status of the result:
-  -	Trying for point
+- For each roll show the value for each die and the sum.  
+- For case i or ii above, indicate that the user either crapped out or won with a natural and then prompt to play again.
+- For case iii indicate that the sum is now the point.  For each subsequent roll indicate the status of the result:
+  - Trying for point
   - Made point and won.  (Prompt user to play again.)
   - Got a seven and lost   (Prompt user to play again.)
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+import java.util.Random;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    private static final Random rand = new Random(); // A shared Random instance
+
+    public static void main(String[] args) {
+        int crapsRoll;
+        boolean playAgain = true;
+        String gameResult;
+        Scanner pipe = new Scanner(System.in);
+
+        System.out.println("Welcome to Craps!");
+        
+        while (playAgain) {
+            System.out.println("Rolling the dice!");
+            crapsRoll = rollDice();
+            System.out.println("You rolled a " + crapsRoll + "!");
+
+            if (crapsRoll == 7 || crapsRoll == 11) {
+                gameResult = "That's a natural! Congratulations, you win!";
+            } else if (crapsRoll == 2 || crapsRoll == 3 || crapsRoll == 12) {
+                gameResult = "That's a craps! Sorry, you lose.";
+            } else {
+                gameResult = rollForPoint(crapsRoll);
+            }
+
+            System.out.println(gameResult);
+
+            playAgain = checkPlayAgain(pipe);
         }
+        System.out.println("Thanks for playing Craps! Goodbye!");
+
+        pipe.close();
+    }
+
+    private static boolean checkPlayAgain(Scanner pipe) {
+        return SafeInput.getYNConfirm(pipe, "Would you like to play again?");
+    }
+
+    private static String rollForPoint(int crapsRoll) {
+        System.out.println("Rolling for a point!");
+        while (true) {
+            System.out.print("Rolling the dice!\t");
+            int pointRoll = rollDice();
+            System.out.print("You rolled a " + pointRoll + "!\n");
+            if (pointRoll == 7) {
+                return "That's a craps! Sorry, you lose.";
+            } else if (pointRoll == crapsRoll) {
+                return "That's a point! Congratulations, you win!";
+            }
+        }
+    }
+
+    private static int rollDice() {
+        int die1 = rollDie();
+        int die2 = rollDie();
+        return die1 + die2;
+    }
+
+    private static int rollDie() {
+        return rand.nextInt(6) + 1;
     }
 }
